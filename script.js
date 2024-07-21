20fa227dbc95d1a81274ac1bb4a9b7b0552f6788
Leo <leocasti2@gmail.com>
2024-07-21 01:25:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Lenguajes
+let lenguajes = ["HTML", "CSS", "JavaScript", "PHP"];
+
+function MostrarLenguajes() {
+  for (let index = 0; index < lenguajes.length; index++) {
+    console.log(lenguajes[i]);
+  }
+}
+
+MostrarLenguajes();
