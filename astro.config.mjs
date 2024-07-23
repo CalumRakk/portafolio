cdfb93775b2dff3cb1989190e7165836bd99e407
Leo <leocasti2@gmail.com>
2024-07-23 11:12:51 -0500

    chore: crear y modificar acciones de GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  site: "https://CalumRakk.github.io",
+  base: "mi-repo",
 });
