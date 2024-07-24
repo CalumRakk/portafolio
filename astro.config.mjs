be5972273222c3a470e4a8321706f5abbb9ec165
Leo <leocasti2@gmail.com>
2024-07-24 01:16:09 -0500

    chore: añade el adaptador para SSR para hacer deploy en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   site: "https://CalumRakk.github.io",
   base: "mi-repo",
+  output: "server",
+  adapter: vercel()
 });
