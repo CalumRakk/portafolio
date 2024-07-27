0aff3344b80dcef575eede92d2c2472882122da9
Leo <leocasti2@gmail.com>
2024-07-27 14:41:42 -0500

    chore: modifica archivos de configuración y elimina comentarios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  site: "https://CalumRakk.github.io",
-  base: "mi-repo",
 });
